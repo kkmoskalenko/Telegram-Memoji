20f3318e70b3f34a8aee0723fa5060a7e5d50c97
Konstantin <kkmoskalenko@icloud.com>
2021-06-28 20:25:03 +0700

    Initial commit

---
+++ Telegram-Memoji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegram-Memoji
+//
+//  Created by Konstantin Moskalenko on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
