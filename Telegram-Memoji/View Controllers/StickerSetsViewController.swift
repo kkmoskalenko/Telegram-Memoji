62f266e236766202e62655e8f9bf4f3d3229b26a
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2021-06-30 11:51:32 +0700

    Configure the initial view controller

---
+++ Telegram-Memoji/View Controllers/StickerSetsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StickerSetsViewController.swift
+//  Telegram-Memoji
+//
+//  Created by Konstantin Moskalenko on 28.06.2021.
+//
+
+import UIKit
+
+final class StickerSetsViewController: UITableViewController {
+    
+}
