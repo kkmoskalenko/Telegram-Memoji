c20ee548088f3341dec24ad48b6842f36704100f
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2021-07-02 11:12:09 +0700

    Add an Add button to the sticker collection view

--- Telegram-Memoji/View Controllers/StickerCollectionViewController.swift
+++ Telegram-Memoji/View Controllers/StickerCollectionViewController.swift
@@ -72,30 +72,51 @@ extension StickerCollectionViewController {
 // MARK: - UICollectionViewDataSource
 
 extension StickerCollectionViewController {
-    private static let reuseIdentifier = "StickerCell"
+    private static let stickerCellReuseIdentifier = "StickerCell"
+    private static let addButtonReuseIdentifier = "AddButtonCell"
     
     override func collectionView(
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int
-    ) -> Int { stickerSet?.stickers?.count ?? 0 }
+    ) -> Int {
+        let stickerCount = stickerSet?.stickers?.count
+        return (stickerCount ?? 0) + 1
+    }
     
     override func collectionView(
         _ collectionView: UICollectionView,
         cellForItemAt indexPath: IndexPath
     ) -> UICollectionViewCell {
+        if indexPath.item == 0 {
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: Self.addButtonReuseIdentifier,
+                for: indexPath) as! AddButtonCell
+            return cell
+        }
+        
         let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: Self.reuseIdentifier,
+            withReuseIdentifier: Self.stickerCellReuseIdentifier,
             for: indexPath) as! StickerImageCell
         
-        guard indexPath.section == 0 else { return cell }
-        
         if let sticker = stickerSet?.stickers?
-            .object(at: indexPath.item) as? Sticker,
+            .object(at: indexPath.item - 1) as? Sticker,
            let imageData = sticker.imageData,
            let image = UIImage(data: imageData)
         { cell.imageView.image = image }
         
         return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension StickerCollectionViewController {
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
+        guard indexPath.item == 0 else { return }
+        presentStickerInputViewController()
     }
 }
 
@@ -142,8 +163,57 @@ extension StickerCollectionViewController: UICollectionViewDelegateFlowLayout {
     ) -> CGFloat { Self.cellSpacing }
 }
 
-// MARK: - StickerImageCell
+// MARK: - Collection View Cells
 
 final class StickerImageCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
+}
+
+final class AddButtonCell: UICollectionViewCell {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        layer.masksToBounds = true
+        layer.backgroundColor = tintColor
+            .withAlphaComponent(0.15).cgColor
+        layer.borderColor = tintColor
+            .withAlphaComponent(0.25).cgColor
+        layer.borderWidth = 1
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = 0.5 * bounds.width
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        alpha = 1
+        UIView.animate(
+            withDuration: 0.35, delay: 0,
+            options: .curveLinear
+        ) { self.alpha = 0.5 }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        alpha = 0.5
+        UIView.animate(
+            withDuration: 0.35, delay: 0,
+            options: .curveLinear
+        ) { self.alpha = 1 }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        
+        alpha = 0.5
+        UIView.animate(
+            withDuration: 0.35, delay: 0,
+            options: .curveLinear
+        ) { self.alpha = 1 }
+    }
 }
